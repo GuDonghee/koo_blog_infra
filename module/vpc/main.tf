91c4d92be6a94ab6b706918cf711a5851c71495c
GuDonghee <dev.donghee1995@gmail.com>
2024-01-27 14:14:27 +0900

    refactor: vpc자원의 하드코딩된 설정 값을 variables.tf파일에 변수화

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -1,34 +1,35 @@
 resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Name      = "koo-blog"
+    Name      = var.vpc_name
     ManagedBy = "Terraform"
   }
 }
 
 resource "aws_subnet" "public" {
+  count      = length(var.public_subnets.cidrs)
   vpc_id     = aws_vpc.this.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.public_subnets.cidrs[count.index]
 
   map_public_ip_on_launch = true
-  availability_zone       = "ap-northeast-2a"
+  availability_zone       = var.public_subnets.azs[count.index]
 
   tags = {
-    Name      = "koo-blog-public-subnet"
+    Name      = "${var.vpc_name}-public-subnet-${count.index}"
     ManagedBy = "Terraform"
   }
 }
 
 resource "aws_subnet" "private" {
-  count      = 2
+  count      = length(var.private_subnets.cidrs)
   vpc_id     = aws_vpc.this.id
   cidr_block = var.private_subnets.cidrs[count.index]
 
   availability_zone = var.private_subnets.azs[count.index]
 
   tags = {
-    Name      = "koo-blog-private-subnet-${count.index}"
+    Name      = "${var.vpc_name}-private-subnet-${count.index}"
     ManagedBy = "Terraform"
   }
 }
@@ -38,7 +39,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.this.id
 
   tags = {
-    Name      = "koo-blog-internetgateway"
+    Name      = "${var.vpc_name}-igw"
     ManagedBy = "Terraform"
   }
 }
--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -1,3 +1,13 @@
+variable "vpc_cidr" {
+  description = "VPC의 IPv4 CIDR 입니다."
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "VPC의 이름 입니다."
+  type        = string
+}
+
 variable "public_subnets" {
   description = "Public 서브넷의 설정 값 목록입니다."
   type        = map(list(string))
