bef067d23a3a3b745169001ad4f2133a8ac3b0f8
GuDonghee <dev.donghee1995@gmail.com>
2024-01-27 14:26:52 +0900

    feat: public, private 서브넷의 RouteTable 설정에 대한 Terraform 소스코드 생성

---
+++ module/vpc/routes.tf
@@ -0,0 +1,38 @@
+resource "aws_route_table" "public" {
+  count  = length(var.public_subnets.cidrs) == 0 ? 0 : 1
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name      = "${var.vpc_name}-public-rt"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnets.cidrs) == 0 ? 0 : 1
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name      = "${var.vpc_name}-private-rt"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets.cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public[0].id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets.cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[0].id
+}
+
+resource "aws_route" "public" {
+  count                  = length(var.public_subnets.cidrs)
+  route_table_id         = aws_route_table.public[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw[0].id
+}
