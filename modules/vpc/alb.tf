2129094908baf3aca5647599823bc2617d8f4e8b
GuDonghee <dev.donghee1995@gmail.com>
2024-02-09 17:06:03 +0900

    feat: 로드밸런싱을 위한 타켓그룹 생성, HTTP 통신을 위한 리스너와 규칙 생성

--- modules/vpc/alb.tf
+++ modules/vpc/alb.tf
@@ -35,7 +35,69 @@ resource "aws_security_group" "alb" {
   }
 
   tags = {
-    Name      = "${var.vpc_name}-application-security"
+    Name      = "${var.vpc_name}-alb-security"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_lb_target_group" "alb" {
+
+  name        = "${var.vpc_name}-alb-target-group"
+  port        = "80"
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.koo-blog.id
+
+  health_check {
+    path                = "/"
+    matcher             = "200-299"
+    unhealthy_threshold = 10
+  }
+
+  tags = {
+    Name      = "${var.vpc_name}-alb-target-group"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "지원하지 않는 CONTENT-TYPE 헤더 형식입니다."
+      status_code  = "400"
+    }
+  }
+
+  tags = {
+    Name      = "${var.vpc_name}-alb-http-listener"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_lb_listener_rule" "http" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 1
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
+
+  tags = {
+    Name      = "${var.vpc_name}-alb-http-listener-rule"
     ManagedBy = "Terraform"
   }
 }
